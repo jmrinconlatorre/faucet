b83028edbc0662a929f0095af00d75cf87b0fcce
jmrinconlatorre <jm.rincon.latorre@gmail.com>
2024-12-17 18:10:38 +0100

    feature: added Tailwind

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
