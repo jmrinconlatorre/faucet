6def8f6a584075853f84c08c590167db0c31f06d
jmrinconlatorre <jm.rincon.latorre@gmail.com>
2025-01-13 12:10:48 +0100

    feat: added get and post route

--- back/index.ts
+++ back/index.ts
@@ -1,21 +1,25 @@
-import express from "express";
-import { Request, Response } from "express";
-import { request } from "http";
+import express from 'express'
+import { Request, Response } from 'express'
+import { request } from 'http'
+import cors from 'cors'
 
-const app = express();
-const port = 3333;
-app.use(express.json());
+const app = express()
+const port = 3333
 
-app.get("/:p1/:p2", (req: Request, res: Response) => {
-  const { p1, p2 } = req.params;
-  res.send(`Hello from ${p1} and ${p2}`);
-});
+app.use(express.json())
+app.use(cors())
 
-app.post("/", (req: Request, res: Response) => {
-  const body = req.body;
-  res.send(`{"message: "Hello from POST", "body": ${JSON.stringify(body)}`);
-});
+app.get('/:p1/:p2', (req: Request, res: Response) => {
+  const { p1, p2 } = req.params
+  res.send({ p1: p1, p2: p2 })
+})
+
+app.post('/', (req: Request, res: Response) => {
+  const body = req.body
+  //res.send(`{ "message": "Hello from POST", "body": ${JSON.stringify(body)} }`)
+  res.send(body)
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,25 @@
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from 'react'
+
+type Data = {
+  p1: string
+  p2: string
+}
 
 export default function Home() {
+  const [data, setData] = useState<Data | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/1111/2222')
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, [])
+
+  if (!data) return <div>Loading...</div>
+
   return (
     <div>
-      <Button>Click me</Button>
+      {data.p1}
+      {data.p2}
     </div>
-  );
+  )
 }
