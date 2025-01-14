9be26101404e26350d929b3dbb0ce06419bcfd73
jmrinconlatorre <jm.rincon.latorre@gmail.com>
2025-01-14 19:25:40 +0100

    feat: added ethereum node in front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,33 @@
 import { useEffect, useState } from 'react'
 
 type Data = {
-  p1: string
-  p2: string
+  jsonrpc: string
+  id: number
+  result: string
 }
 
 export default function Home() {
   const [data, setData] = useState<Data | null>(null)
 
   useEffect(() => {
-    fetch('http://localhost:3333/1111/2222')
+    fetch('http://localhost:5556', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'eth_getBalance',
+        params: ['0x3b30E0F1883a37B7cF3894a7a43eF040E31B545F', 'latest'],
+        id: 1,
+      }),
+    })
       .then(res => res.json())
-      .then(data => setData(data))
+      .then(setData)
+      .catch(console.error)
   }, [])
 
   if (!data) return <div>Loading...</div>
 
-  return (
-    <div>
-      {data.p1}
-      {data.p2}
-    </div>
-  )
+  return <div>{Number(data.result)}</div>
 }
