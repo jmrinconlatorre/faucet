974545fcb4ea9d6a5d68b6773b0f20d8f336b25c
jmrinconlatorre <jm.rincon.latorre@gmail.com>
2025-01-16 17:12:10 +0100

    fix: rewrite front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,9 @@
-import { useEffect, useState } from 'react'
-
-type Data = {
-  jsonrpc: string
-  id: number
-  result: string
-}
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  const [data, setData] = useState<Data | null>(null)
-
-  useEffect(() => {
-    fetch('http://localhost:5556', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        method: 'eth_getBalance',
-        params: ['0x3b30E0F1883a37B7cF3894a7a43eF040E31B545F', 'latest'],
-        id: 1,
-      }),
-    })
-      .then(res => res.json())
-      .then(setData)
-      .catch(console.error)
-  }, [])
-
-  if (!data) return <div>Loading...</div>
-
-  return <div>{Number(data.result)}</div>
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
